4fc3ba6fc55392a02ece0269a04d2ea5fda4b48a
angelaavr <48030956+angelaavr@users.noreply.github.com>
2019-03-18 22:39:04 +0100

    initial commit
    homework

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int answer;
+            Console.WriteLine("Enter the First number:");
+            var number1 = Console.ReadLine();
+            int firstNum;
+            if (int.TryParse(number1, out firstNum))
+            {
+                Console.WriteLine(firstNum);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input numbers, please try again");
+            }
+
+            Console.WriteLine("Enter the Second number:");
+            var number2 = Console.ReadLine();
+            int secondNum;
+            if (int.TryParse(number2, out secondNum))
+            {
+                Console.WriteLine(secondNum);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input numbers, please try again");
+
+            }
+
+
+            Console.WriteLine("Enter the operation ( + , - , * , / ) ");
+            string operation = Console.ReadLine();
+
+            if (operation == "*")
+            {
+                answer = firstNum * secondNum;
+                Console.WriteLine(firstNum + " * " + secondNum + " = " + answer);
+                Console.ReadLine();
+            }
+            else if (operation == "/")
+            {
+                answer = firstNum / secondNum;
+                Console.WriteLine(firstNum + " / " + secondNum + " = " + answer);
+                Console.ReadLine();
+            }
+            else if (operation == "+")
+            {
+                answer = firstNum + secondNum;
+                Console.WriteLine(firstNum + " + " + secondNum + " = " + answer);
+                Console.ReadLine();
+            }
+            else if (operation == "-")
+            {
+                answer = firstNum - secondNum;
+                Console.WriteLine(firstNum + " - " + secondNum + " = " + answer);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Invalid operation selected, please try again.");     
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
